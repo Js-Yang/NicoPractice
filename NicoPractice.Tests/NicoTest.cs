00db607f520de35958b5c53647c303a88cc545b9
jason.yang <chyang590@gmail.com>
2017-11-06 00:46:49 +0800

    add test case "Nico_When_Key_is_bac_And_Message_is_abcdef_should_return_abcd  ", and the case failed

--- NicoPractice.Tests/NicoTest.cs
+++ NicoPractice.Tests/NicoTest.cs
@@ -12,6 +12,7 @@ namespace NicoPractice.Tests
         [TestCase("abc","abc","abc",TestName = "Nico_When_Key_is_abc_And_Message_is_abc_should_return_abc")]
         [TestCase("bac","abc","bac",TestName = "Nico_When_Key_is_bac_And_Message_is_abc_should_return_bac")]
         [TestCase("abc", "abcdef", "abcdef", TestName = "Nico_When_Key_is_bac_And_Message_is_abcdef_should_return_abcdef")]
+        [TestCase("abc", "abcd", "abcd  ", TestName = "Nico_When_Key_is_bac_And_Message_is_abcdef_should_return_abcd  ")]
         public void Basic_Nico_Variation(string key, string message, string expectedResult)
         {
             var nico = new Nico();
