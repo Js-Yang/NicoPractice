1ee19729072fe8c05804a90e246cbbf78e110cfc
jason.yang <chyang590@gmail.com>
2017-11-06 00:45:49 +0800

    let test case Nico_When_Key_is_bac_And_Message_is_abcdef_should_return_abcdef PASS

--- NicoPractice/Nico.cs
+++ NicoPractice/Nico.cs
@@ -1,24 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NicoPractice
 {
     public class Nico
     {
-        public string NicoVariation(string key, string message)
+        public string NicoVariation(string letters, string message)
         {
-            char[] letters = key.ToCharArray();
-            var container = new Dictionary<char, char>();
-            for (var indexOfLetters = 0; indexOfLetters < letters.Length; indexOfLetters++)
-            {
-                container.Add(letters[indexOfLetters], message.ToCharArray()[indexOfLetters]);
-            }
+            var keys = letters.ToCharArray();
+            var content = message.ToCharArray();
+            var container = GetContainer(keys, content);
 
-            var encryptsContainer = container.OrderBy(x => x.Key);
-            return string.Join("", encryptsContainer.Select(x => x.Value).ToArray());
+            return GetCyphertext(keys, content, container.OrderBy(x => x.Key));
+        }
+
+        private static string GetCyphertext(char[] keys, char[] content, IOrderedEnumerable<KeyValuePair<char, List<char>>> encryptsContainer)
+        {
+            var result = string.Empty;
+            var columnCount = keys.Length;
+            var rowCount = (content.Length / keys.Length);
+            for (var indexOfRow = 0; indexOfRow < rowCount; indexOfRow++)
+            {
+                for (var indexOfColumn = 0; indexOfColumn < columnCount; indexOfColumn++)
+                {
+                    result = string.Concat(result, encryptsContainer.ElementAt(indexOfColumn).Value[indexOfRow]);
+                }
+            }
+            return result;
+        }
+
+        private static Dictionary<char, List<char>> GetContainer(char[] keys, char[] content)
+        {
+            var container = new Dictionary<char, List<char>>();
+            var indexOfLetters = 0;
+            foreach (var value in content)
+            {
+                var key = keys[indexOfLetters++];
+                if (container.ContainsKey(key))
+                {
+                    container[key].Add(value);
+                }
+                else
+                {
+                    container.Add(key, new List<char>() { value });
+                }
+                
+                if (indexOfLetters == keys.Length)
+                {
+                    indexOfLetters = 0;
+                }
+            }
+            return container;
         }
     }
 }
