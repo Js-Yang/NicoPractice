59cfe832adb84f9a5c68acf4960c41efbd71fbfb
jason.yang <chyang590@gmail.com>
2017-11-06 01:42:28 +0800

    remove unnesassary parameter

--- NicoPractice/Nico.cs
+++ NicoPractice/Nico.cs
@@ -11,19 +11,20 @@ namespace NicoPractice
         {
             var keys = letters.ToCharArray();
             var alphabets = message.ToCharArray();
-            var container = GetRelationShip(keys, alphabets);
-            return GetCyphertext(keys, alphabets, container.OrderBy(x => x.Key));
+            var combination = GetRelationShip(keys, alphabets);
+            return GetCyphertext(combination.OrderBy(x => x.Key));
         }
 
-        private static string GetCyphertext(char[] keys, char[] content, IOrderedEnumerable<KeyValuePair<char, List<char>>> encryptsContainer)
+        private static string GetCyphertext(IOrderedEnumerable<KeyValuePair<char, List<char>>> encryptsContainer)
         {
             var cyphertext = string.Empty;
-            var columnCount = keys.Length;
-            var rowCount = GetRowsCount(keys, content);
+            var keysCount = encryptsContainer.Count();
+            var alphabetCount = encryptsContainer.Sum(keyValuePair => keyValuePair.Value.Count);
+            var rowCount = GetRowsCount(keysCount, alphabetCount);
 
             for (var indexOfRow = 0; indexOfRow < rowCount; indexOfRow++)
             {
-                for (var indexOfColumn = 0; indexOfColumn < columnCount; indexOfColumn++)
+                for (var indexOfColumn = 0; indexOfColumn < keysCount; indexOfColumn++)
                 {
                     cyphertext = string.Concat(cyphertext, GetAlphabet(encryptsContainer, indexOfColumn, indexOfRow));
                 }
@@ -41,11 +42,11 @@ namespace NicoPractice
             return alphabet;
         }
 
-        private static int GetRowsCount(char[] keys, char[] content)
+        private static int GetRowsCount(int keysLength, int rowsCount)
         {
-            return (content.Length % keys.Length) == 0
-                ? (content.Length / keys.Length)
-                : (content.Length / keys.Length) + 1;
+            return (rowsCount % keysLength) == 0
+                ? (rowsCount / keysLength)
+                : (rowsCount / keysLength) + 1;
         }
 
         private static Dictionary<char, List<char>> GetRelationShip(char[] keys, char[] content)
