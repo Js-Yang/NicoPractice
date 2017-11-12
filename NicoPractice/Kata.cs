02d38668a42bf4ef56bde33d452c091ee6c5c0b3
jason.yang <chyang590@gmail.com>
2017-11-13 00:16:10 +0800

    rename method

--- NicoPractice/Kata.cs
+++ NicoPractice/Kata.cs
@@ -2,35 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Solution
+public class Kata
 {
-    public class Kata
+    public static string Nico(string keys, string message)
     {
-        public static string Nico(string keys, string message)
-        {
-            var paragraphs = GetParagraphsByKeySize(message, keys.Length);
+        var paragraphs = GetParagraphsBy(message, keys.Length);
 
-            return paragraphs.Aggregate(string.Empty, (cipherText, paragraph) => cipherText + Encrypts(keys, paragraph));
-        }
+        return paragraphs.Aggregate(string.Empty, (cipherText, paragraph) => cipherText + Encrypts(keys, paragraph));
+    }
 
-        private static string Encrypts(string keys, string message)
-        {
-            var index = 0;
-            return GetCipherText(keys.ToDictionary(key => key, key => message[index++]));
-        }
+    private static string Encrypts(string keys, string message)
+    {
+        var index = 0;
+        return GetCipherText(keys.ToDictionary(key => key, key => message[index++]));
+    }
 
-        private static IEnumerable<string> GetParagraphsByKeySize(string text, int chunkSize)
-        {
-            var textLengthNeeded = (int)(Math.Ceiling((double)text.Length / chunkSize) * chunkSize);
+    private static IEnumerable<string> GetParagraphsBy(string text, int chunkSize)
+    {
+        var textLengthNeeded = (int)(Math.Ceiling((double)text.Length / chunkSize) * chunkSize);
 
-            text = text.PadRight(textLengthNeeded, ' ');
+        text = text.PadRight(textLengthNeeded, ' ');
 
-            return Enumerable.Range(0, text.Length / chunkSize).Select(i => text.Substring(i * chunkSize, chunkSize));
-        }
+        return Enumerable.Range(0, text.Length / chunkSize).Select(i => text.Substring(i * chunkSize, chunkSize));
+    }
 
-        private static string GetCipherText(Dictionary<char, char> keysMessage)
-        {
-            return string.Concat(keysMessage.OrderBy(x => x.Key).Select(x => x.Value).ToArray());
-        }
+    private static string GetCipherText(Dictionary<char, char> keysMessage)
+    {
+        return string.Concat(keysMessage.OrderBy(x => x.Key).Select(x => x.Value).ToArray());
     }
 }
