6d17701c8db1a16fffd9797d296a5c63f329faf4
jason.yang <chyang590@gmail.com>
2017-11-13 00:15:15 +0800

    revert version and rename method

--- NicoPractice/Kata.cs
+++ NicoPractice/Kata.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
-public class Kata
+namespace Solution
 {
-    public static string Nico(string keys, string message)
+    public class Kata
     {
-        var sizeOfParagraph = (int)Math.Ceiling((double)message.Length / keys.Length);
-        var plainText = message.PadRight((sizeOfParagraph * keys.Length), ' ');
-        return Enumerable.Range(0, sizeOfParagraph).Select(size => plainText.Substring(size * keys.Length, keys.Length)).Aggregate(string.Empty, (cipherText, paragraph) =>
+        public static string Nico(string keys, string message)
+        {
+            var paragraphs = GetParagraphsByKeySize(message, keys.Length);
+
+            return paragraphs.Aggregate(string.Empty, (cipherText, paragraph) => cipherText + Encrypts(keys, paragraph));
+        }
+
+        private static string Encrypts(string keys, string message)
         {
             var index = 0;
-            return cipherText + string.Concat(keys.ToDictionary(key => key, key => paragraph[index++]).OrderBy(x => x.Key).Select(x => x.Value).ToArray());
-        });
+            return GetCipherText(keys.ToDictionary(key => key, key => message[index++]));
+        }
+
+        private static IEnumerable<string> GetParagraphsByKeySize(string text, int chunkSize)
+        {
+            var textLengthNeeded = (int)(Math.Ceiling((double)text.Length / chunkSize) * chunkSize);
+
+            text = text.PadRight(textLengthNeeded, ' ');
+
+            return Enumerable.Range(0, text.Length / chunkSize).Select(i => text.Substring(i * chunkSize, chunkSize));
+        }
+
+        private static string GetCipherText(Dictionary<char, char> keysMessage)
+        {
+            return string.Concat(keysMessage.OrderBy(x => x.Key).Select(x => x.Value).ToArray());
+        }
     }
 }
